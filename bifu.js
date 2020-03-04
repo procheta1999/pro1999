8c10714fc19589808df8ec9826b6e39c99e143d5
procheta1999 <53190270+procheta1999@users.noreply.github.com>
2020-03-04 19:36:18 +0530

    Add files via upload

---
+++ bifu.js
@@ -0,0 +1,9 @@
+var myWindow;
+
+function openWin() {
+  myWindow = window.alert("BIFU is a biodiesel made from used cooking oil, collected from household and various eateries.It is currently under MSME funding.");
+}
+
+function closeWin() {
+  myWindow.close();
+}
