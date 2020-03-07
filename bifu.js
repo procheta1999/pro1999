fe4a9edbcf0bab261ecf4fa1bbab01bb76812d42
procheta1999 <53190270+procheta1999@users.noreply.github.com>
2020-03-07 21:41:18 +0530

    Delete bifu.js

--- bifu.js
+++
@@ -1,9 +0,0 @@
-var myWindow;
-
-function openWin() {
-  myWindow = window.alert("BIFU is a biodiesel made from used cooking oil, collected from household and various eateries.It is currently under MSME funding.");
-}
-
-function closeWin() {
-  myWindow.close();
-}
